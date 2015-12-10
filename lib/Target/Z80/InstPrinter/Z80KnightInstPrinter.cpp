0cfa2340287aeca779bd92f7d5f7e44360b5e75b
William Gulian <williamgulian@gmail.com>
2015-12-09 22:29:14 -0500

    Fix another syntax error

--- lib/Target/Z80/InstPrinter/Z80KnightInstPrinter.cpp
+++ lib/Target/Z80/InstPrinter/Z80KnightInstPrinter.cpp
@@ -30,7 +30,7 @@ void Z80KnightInstPrinter::printInst(const MCInst *MI, raw_ostream &O,
     
     if (MI->getOpcode() == Z80::LD16ri && 
             MI->getNumOperands() == 2 && 
-            MI->getOperand(1)->isExpr() && 
+            MI->getOperand(1).isExpr() && 
             MI->getOperand(1).getExpr()->getKind() == MCExpr::SymbolRef) {
         O << "\tkld(";
         printOperand(MI, 0, O);
